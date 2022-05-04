83b31003c762f1365ab0577df04574b1652ec32b
Enrico Willemse <enricowillemse.was@gmail.com>
2022-05-04 05:06:45 +0000

    Update

--- src/smart-task-component.vue
+++ src/smart-task-component.vue
@@ -1,169 +1,150 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-// @ts-ignore
-import { VuePaycard } from "vue-paycard";
+import { defineComponent } from "vue";
+import { StripeCheckout } from "@vue-stripe/vue-stripe";
+
+export interface IStateModel {
+  priceId: string;
+  price: number;
+  currency: string;
+  productName: string;
+  productDescription: string;
+}
 
 export default /*#__PURE__*/ defineComponent({
   name: "SmartTaskComponent", // vue component name
   props: ["componentState", "componentStatus"],
   components: {
-    VuePaycard,
+    StripeCheckout,
   },
   data() {
     return {
       model: {
-        cardName: "",
-        cardNumber: "",
-        cardMonth: "",
-        cardYear: "",
-        cardCvv: "",
-      },
+        priceId: "price_1KvZy2LmsBhsnW8s4fC3qwGP",
+        price: 20,
+        currency: "SGD",
+        productName: "Test Product",
+        productDescription: "Test Product Description",
+      } as IStateModel,
+      publishableKey:
+        "pk_test_51KvZrULmsBhsnW8smgaE7G0xPcUTqGKLeSdNAwdNcVsFUcy56Eqec96kqHLdshFoCWzFq2r0ycX82cyRT684EVQv002mMCy5ZN",
+      loading: false,
+      lineItems: [
+        {
+          price: "price_1KvZy2LmsBhsnW8s4fC3qwGP", // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: "https://myworkflows.io",
+      cancelURL: "https://myworkflows.io",
     };
   },
-  computed: {},
   methods: {
-    async submitForm() {
-      this.$emit("execute-smart-task", this.model);
+    submit() {
+      // You will be redirected to Stripe's secure checkout page
+      (this.$refs.checkoutRef as any).redirectToCheckout();
     },
-    pay() {
-      // Get stripe element
-      const cardElement = this.card.stripeElement;
-
-      // Access instance methods, e.g. createToken()
-      this.elms.instance.createToken(cardElement).then((result: object) => {
-        // Handle result.error or result.token
-        console.log(result);
-      });
-    },
-  },
-  created() {
-    if (this.componentState) {
-      this.model = this.componentState;
-    }
-  },
-  setup() {
-    const stripeKey = ref("pk_test_TYooMQauvdEDq54NiTphI7jx"); // test key
-    const instanceOptions = ref({
-      // https://stripe.com/docs/js/initializing#init_stripe_js-options
-    });
-    const elementsOptions = ref({
-      // https://stripe.com/docs/js/elements_object/create#stripe_elements-options
-    });
-    const cardOptions = ref({
-      // https://stripe.com/docs/stripe.js#element-options
-      value: {
-        postalCode: "12345",
-      },
-    });
-    const stripeLoaded = ref(false);
-    const card = ref();
-    const elms = ref();
-
-
-    return {
-      stripeKey,
-      stripeLoaded,
-      instanceOptions,
-      elementsOptions,
-      cardOptions,
-      card,
-      elms,
-    };
   },
 });
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-6">
-      <VuePaycard :value-fields="model" />
+  <div>
+    <stripe-checkout
+      ref="checkoutRef"
+      mode="payment"
+      :pk="publishableKey"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="(v) => (loading = v)"
+    />
+    <div class="card-plan">
+      <div class="card-plan-img">
+        <img
+        height="90"
+          src="https://cdn2.iconfinder.com/data/icons/e-commerce-line-4-1/1024/open_box4-512.png"
+          alt=""
+        />
+      </div>
+      <div class="card-plan-text">
+        <div class="card-plan-title">{{ model.productName }}</div>
+        <p>{{ model.productDescription }}</p>
+        <div class="card-plan-price">
+          {{ model.price }} {{ model.currency }}
+        </div>
+      </div>
     </div>
-    <div class="col-6">
-      <div class="row  mb-2">
-        <div class="col-sm-12">
-          <div class="form-group">
-            <label for="name">Name</label>
-            <input
-              v-model="model.cardName"
-              class="form-control"
-              id="name"
-              type="text"
-              placeholder="Enter your name"
-            />
-          </div>
-        </div>
-      </div>
-      <div class="row mb-2">
-        <div class="col-sm-12">
-          <div class="form-group">
-            <label for="ccnumber">Credit Card Number</label>
-            <div class="input-group">
-              <input
-                v-model="model.cardNumber"
-                class="form-control"
-                type="text"
-                placeholder="0000 0000 0000 0000"
-                autocomplete="cc-number"
-              />
-              <div class="input-group-append">
-                <span class="input-group-text">
-                  <i class="bi bi-credit-card"></i>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row  mb-2">
-        <div class="form-group col-sm-4">
-          <label for="ccmonth">Month</label>
-          <select v-model="model.cardMonth" class="form-control" id="ccmonth">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-            <option>11</option>
-            <option>12</option>
-          </select>
-        </div>
-        <div class="form-group col-sm-4">
-          <label for="ccyear">Year</label>
-          <select v-model="model.cardYear" class="form-control" id="ccyear">
-            <option>2014</option>
-            <option>2015</option>
-            <option>2016</option>
-            <option>2017</option>
-            <option>2018</option>
-            <option>2019</option>
-            <option>2020</option>
-            <option>2021</option>
-            <option>2022</option>
-            <option>2023</option>
-            <option>2024</option>
-            <option>2025</option>
-          </select>
-        </div>
-        <div class="col-sm-4">
-          <div class="form-group">
-            <label for="cvv">CVV/CVC</label>
-            <input
-              class="form-control"
-              id="cvv"
-              type="text"
-              placeholder="123"
-            />
-          </div>
-        </div>
-      </div>
-      <div class="row mx-1">
-        <button class="btn btn-primary">Pay</button>
-      </div>
+    <div class="card-payment-button">
+      <button class="btn btn-info" @click="submit">Proceed to Payment</button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.card .card-body .card-payment-button {
+  padding: 25px 0px 15px;
+  box-sizing: border-box;
+}
+.card .card-body .card-payment-button button {
+  width: 100%;
+  height: 50px;
+  border: 0;
+  background: hsl(245, 75%, 52%);
+  color: white;
+  font-weight: 700;
+  border-radius: 10px;
+  box-shadow: 0px 10px 20px 0px hsl(245deg 75% 52% / 44%);
+  cursor: pointer;
+}
+.card .card-body .card-payment-button button:hover {
+  background: #766cf1;
+}
+.card .card-body .card-title {
+  width: 100%;
+  font-weight: 900;
+  color: hsl(223, 47%, 23%);
+  text-align: center;
+  padding: 15px;
+  box-sizing: border-box;
+}
+.card .card-body .card-text {
+  width: 100%;
+  color:hsl(224, 23%, 55%);
+  text-align: center;
+  line-height: 25px;
+  padding: 15px 0px;
+  box-sizing: border-box;
+}
+.card .card-body .card-plan {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 15px;
+  background: hsl(225, 100%, 98%);
+  border-radius: 10px;
+  padding: 15px;
+  box-sizing: border-box;
+}
+.card .card-body .card-plan .card-plan-img {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.card .card-body .card-plan .card-plan-text {
+  flex-grow: 6;
+  display: flex;
+  flex-direction: column;
+  row-gap: 4px;
+}
+.card .card-body .card-plan .card-plan-text .card-plan-title {
+  color:hsl(223, 47%, 23%);
+  font-weight: 900;
+  font-size: 14px;
+}
+.card .card-body .card-plan .card-plan-text .card-plan-price {
+  color: hsl(224, 23%, 55%);
+  font-size: 14px;
+}
+</style>
